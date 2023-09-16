310468ff42dfa9e567991ed02d3efbf29cc9b0ca
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2023-09-16 14:50:38 +0300

    Support Nuitka 1.8 for Windows builds as well

--- nuitka_build.bat
+++ nuitka_build.bat
@@ -1,5 +1,8 @@
 @echo off
+
+SET nuitka_compile_flags=--standalone --onefile --remove-output --no-deployment-flag=self-execution
+
 DEL bin\wookiee_unicaster.exe
-python -m nuitka --standalone --onefile --remove-output wookiee_unicaster.py
+python -m nuitka %nuitka_compile_flags% wookiee_unicaster.py
 MOVE wookiee_unicaster.exe bin\wookiee_unicaster.exe
 
