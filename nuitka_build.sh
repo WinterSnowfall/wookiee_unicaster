f43c1c8fcd776a373f91c9c93c814597213aa508
Winter Snowfall <WinterSnowfall@users.noreply.github.com>
2023-09-16 14:32:58 +0300

    Support Nuitka 1.8 and remove binary builds (resorting to them is usually unnecessary)

--- nuitka_build.sh
+++ nuitka_build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+NUITKA_COMPILE_FLAGS="--standalone --onefile --remove-output --no-deployment-flag=self-execution"
+
 rm bin/wookiee_unicaster
-nuitka3 wookiee_unicaster.py --standalone --onefile --remove-output
+nuitka3 wookiee_unicaster.py $NUITKA_COMPILE_FLAGS
 chmod +x wookiee_unicaster.bin
 mv wookiee_unicaster.bin bin/wookiee_unicaster
 
