64c40ac54680e77745071e5cb0a608de029eea7d
WinterSnowfall <WinterSnowfall@users.noreply.github.com>
2024-11-04 19:42:13 +0200

    Add GitHub actions workflow

--- nuitka_build.bat
+++ nuitka_build.bat
@@ -1,8 +1,9 @@
 @echo off
 
-SET nuitka_compile_flags=--standalone --onefile --remove-output --no-deployment-flag=self-execution
+SET nuitka_compile_flags=--standalone --onefile --remove-output --no-deployment-flag=self-execution --assume-yes-for-downloads
 
-DEL bin\wookiee_unicaster.exe
+DEL bin\blank.tmp 2>nul
+DEL bin\wookiee_unicaster.exe 2>nul
 python -m nuitka %nuitka_compile_flags% wookiee_unicaster.py
 MOVE wookiee_unicaster.exe bin\wookiee_unicaster.exe
 
--- nuitka_build.sh
+++ nuitka_build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-NUITKA_COMPILE_FLAGS="--standalone --onefile --remove-output --no-deployment-flag=self-execution"
+NUITKA_COMPILE_FLAGS="--standalone --onefile --remove-output --no-deployment-flag=self-execution --assume-yes-for-downloads"
 
-rm bin/wookiee_unicaster
-nuitka3 wookiee_unicaster.py $NUITKA_COMPILE_FLAGS
+rm bin/blank.tmp 2>/dev/null
+rm bin/wookiee_unicaster 2>/dev/null
+python3 -m nuitka wookiee_unicaster.py $NUITKA_COMPILE_FLAGS
 chmod +x wookiee_unicaster.bin
 mv wookiee_unicaster.bin bin/wookiee_unicaster
 
